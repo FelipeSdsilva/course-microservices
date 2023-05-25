381328d63d92310027a098844bf6fcf6ca9e73bc
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-25 17:13:32 -0300

    Feign Client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    public RestTemplate registeRestTemplate(){
+    RestTemplate registeRestTemplate() {
         return new RestTemplate();
     }
 
--- hr-worker/src/main/java/com/devsuperior/hrworker/services/exceptions/ResourceNotFoundException.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package com.devsuperior.hrworker.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
         super(message);
     }
 }
