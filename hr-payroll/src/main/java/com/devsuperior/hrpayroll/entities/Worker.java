e209fcdeb428aae69b292f20831bf3d08efb7e36
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-25 16:36:49 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,57 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Worker implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Worker worker)) return false;
+        return getId().equals(worker.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
